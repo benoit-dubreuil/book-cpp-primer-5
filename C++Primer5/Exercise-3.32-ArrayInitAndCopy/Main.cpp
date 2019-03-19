5d8eab85c197e36dd9f60a9b4f85930f47ff3dd3
Benoit Dubreuil <b.dubreuil@hotmail.com>
2019-03-19 16:54:21 -0400

    Added more information that I learnt from the book C++ Primer 5th Edition

--- C++Primer5/Exercise-3.32-ArrayInitAndCopy/Main.cpp
+++ C++Primer5/Exercise-3.32-ArrayInitAndCopy/Main.cpp
@@ -5,7 +5,8 @@ int main()
 {
 	int values[10];
 
-	// This allows to not derefence the first element. It was done only for the purpose of testing.
+	// This allows to not derefence the first element. It was done only for the purpose of testing. 
+	// The reason why it is necessary to remove references is because the * operator on an array is equivalent to &array[0].
 	for (std::size_t i = 0; i < (sizeof(values) / sizeof(std::remove_reference<decltype(*values)>::type)); ++i)
 	{
 		values[i] = i;
